de1238c5d30046d6272b6bcbb49cd7bbbc9fe2ae
LarsHH <lars.h.hertel@gmail.com>
2017-04-20 12:21:04 -0800

    Deployed c39fe92 with MkDocs version: 0.16.3

---
+++ gen_docs.py
@@ -0,0 +1,21 @@
+import re
+from hobbit.algorithms import Hyperband
+from hobbit import Hyperparameter
+
+def process_class_docstring(docstring):
+    docstring = re.sub(r'\n    # (.*)\n',
+                       r'\n    __\1__\n\n',
+                       docstring)
+
+    docstring = re.sub(r'    ([^\s\\]+):(.*)\n',
+                       r'    - __\1__:\2\n',
+                       docstring)
+
+    docstring = docstring.replace('    ' * 5, '\t\t')
+    docstring = docstring.replace('    ' * 3, '\t')
+    docstring = docstring.replace('    ', '')
+    return docstring
+
+open('hyperband.md', 'w').write(process_class_docstring(Hyperband.__doc__))
+open('hyperparameter.md', 'w').write(process_class_docstring(
+    Hyperparameter.__doc__))
