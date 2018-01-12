65dca70cf5cff8929819972004c2048686ab26ca
Lars Hertel <lhertel@uci.edu>
2018-01-12 09:41:51 -0800

    readme with install instructions

--- README.md
+++ README.md
@@ -1,61 +1,31 @@
 
 # SHERPA
 
-Welcome to SHERPA - a hyperparameter tuning framework for machine learning.
-In order to get SHERPA running clone the repository from GitLab by
-calling ```git clone git@gitlab.ics.uci.edu:uci-igb/sherpa.git``` from the
-command line and adding the directory to the Python path (e.g.
-```export PYTHONPATH=$PYTHONPATH:/user/local/sherpa/```). In order to get the
-necessary dependencies you can run ```python setup.py install``` from the SHERPA folder.
+## Installation
+Clone into ```/your/path/``` from GitLab:
+```
+cd /your/path/
+git clone git@gitlab.ics.uci.edu:uci-igb/sherpa.git
+```
 
-### Optional Dependencies
-+ Drmaa 0.7.8 (for SGE)
-+ Keras (for examples)
-+ GPU Lock (for examples and recommended for SGE)
+Add SHERPA and GPU_LOCK to Python-path in your profile:
+```
+export PYTHONPATH=$PYTHONPATH:/your/path/sherpa/
+export PYTHONPATH=$PYTHONPATH:/extra/pjsadows0/libs/shared/gpu_lock/
+```
 
-## Running Sherpa
+Add MongoDB, DRMAA and SGE to your profile:
+```
+source /auto/igb-libs/linux/centos/6.x/x86_64/profiles/general
+export DRMAA_LIBRARY_PATH=/opt/sge/lib/lx-amd64/libdrmaa.so
+module load sge
+```
 
-### Parameters
-Base class for a parameter.
-
-### Algorithm
-Abstract algorithm that returns next parameters conditional on parameter
-ranges and previous results.
-
-### Stopping Rules
-Abstract class to evaluate whether a trial should stop conditional on all
-results so far.
-
-### Combining these into a Study
-A Study defines an entire optimization and its results.
-
-__Attributes:__
-
-- __algorithm__ _(sherpa.algorithms.Algorithm)_: takes results table and returns
-parameter set.
-- __results__ _(pandas.DataFrame)_: contains results from this study.
-- __parameters__ _(list[sherpa.Parameter])_: parameters being optimized.
-- __stopping_rule__ _(sherpa.stopping_rules.StoppingRule)_: rule for stopping
-trials prematurely.
-- __lower_is_better__ _(bool)_: whether lower objective values are better.
+Install dependencies:
+```
+cd /your/path/sherpa
+python setup.py install --parallel
+```
 
 
-### Scheduler
-Abstract interface to a job scheduler.
-
-The job scheduler gives an API to submit jobs and retrieve statuses of all
-jobs.
-
-### Putting it all together
-Runs a Study via the Runner class.
-
-__Arguments:__
-
-- __filename__ _(str)_: the name of the file which is called to evaluate
-configurations
-- __study__ _(sherpa.Study)_: the Study to be run.
-- __output_dir__ _(str)_: where scheduler and database files will be stored.
-- __scheduler__ _(sherpa.Scheduler)_: a scheduler.
-- __max_concurrent__ _(int)_: the number of trials that will be evaluated in
-parallel.
 
--- generate_readme.py
+++ generate_readme.py
@@ -20,45 +20,59 @@ def process_function_docstring(docstring):
 text = """
 # SHERPA
 
-Welcome to SHERPA - a hyperparameter tuning framework for machine learning.
-In order to get SHERPA running clone the repository from GitLab by
-calling ```git clone git@gitlab.ics.uci.edu:uci-igb/sherpa.git``` from the
-command line and adding the directory to the Python path (e.g.
-```export PYTHONPATH=$PYTHONPATH:/user/local/sherpa/```). In order to get the
-necessary dependencies you can run ```python setup.py install``` from the SHERPA folder.
+## Installation
+Clone into ```/your/path/``` from GitLab:
+```
+cd /your/path/
+git clone git@gitlab.ics.uci.edu:uci-igb/sherpa.git
+```
 
-### Optional Dependencies
-+ Drmaa 0.7.8 (for SGE)
-+ Keras (for examples)
-+ GPU Lock (for examples and recommended for SGE)
+Add SHERPA and GPU_LOCK to Python-path in your profile:
+```
+export PYTHONPATH=$PYTHONPATH:/your/path/sherpa/
+export PYTHONPATH=$PYTHONPATH:/extra/pjsadows0/libs/shared/gpu_lock/
+```
 
-## Running Sherpa
+Add MongoDB, DRMAA and SGE to your profile:
+```
+source /auto/igb-libs/linux/centos/6.x/x86_64/profiles/general
+export DRMAA_LIBRARY_PATH=/opt/sge/lib/lx-amd64/libdrmaa.so
+module load sge
+```
+
+Install dependencies:
+```
+cd /your/path/sherpa
+python setup.py install --parallel
+```
+
+
 
 """
 
-text += "### Parameters"
-text += process_function_docstring(sherpa.Parameter.__doc__)
-text += "\n"
-
-text += "### Algorithm"
-text += process_function_docstring(sherpa.algorithms.Algorithm.__doc__)
-text += "\n"
-
-text += "### Stopping Rules"
-text += process_function_docstring(sherpa.algorithms.StoppingRule.__doc__)
-text += "\n"
-
-text += "### Combining these into a Study"
-text += process_function_docstring(sherpa.Study.__doc__)
-text += "\n"
-
-text += "### Scheduler"
-text += process_function_docstring(sherpa.schedulers.Scheduler.__doc__)
-text += "\n"
-
-text += "### Putting it all together"
-text += process_function_docstring(sherpa.optimize.__doc__)
-text += "\n"
+# text += "### Parameters"
+# text += process_function_docstring(sherpa.Parameter.__doc__)
+# text += "\n"
+#
+# text += "### Algorithm"
+# text += process_function_docstring(sherpa.algorithms.Algorithm.__doc__)
+# text += "\n"
+#
+# text += "### Stopping Rules"
+# text += process_function_docstring(sherpa.algorithms.StoppingRule.__doc__)
+# text += "\n"
+#
+# text += "### Combining these into a Study"
+# text += process_function_docstring(sherpa.Study.__doc__)
+# text += "\n"
+#
+# text += "### Scheduler"
+# text += process_function_docstring(sherpa.schedulers.Scheduler.__doc__)
+# text += "\n"
+#
+# text += "### Putting it all together"
+# text += process_function_docstring(sherpa.optimize.__doc__)
+# text += "\n"
 
 
 with open('README.md', 'w') as f:
