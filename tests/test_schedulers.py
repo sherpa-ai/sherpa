4c6a820d09cb0eeb605a46da91adbed9e9be7f92
Lars Hertel <larshertel@dhcp-024050.ics.uci.edu>
2018-04-08 08:27:05 -0700

    schedulers tests file

---
+++ tests/test_schedulers.py
@@ -0,0 +1,68 @@
+import os
+import sherpa
+import sherpa.schedulers
+import socket
+import tempfile
+import time
+import logging
+import shutil
+
+
+logging.basicConfig(level=logging.DEBUG)
+testlogger = logging.getLogger(__name__)
+
+def test_sge_scheduler():
+    host = socket.gethostname()
+    if (not host == "nimbus") and (not host.startswith('arcus')):
+        return
+
+    test_dir = tempfile.mkdtemp(dir=".")
+
+    trial_script = "import time, os\n"
+    trial_script += "assert os.environ.get('SHERPA_TRIAL_ID') == '3'\n"
+    trial_script += "assert os.environ.get('SHERPA_HOST') == {}\n".format(host)
+    trial_script += "time.sleep(5)\n"
+
+    with open(os.path.join(test_dir, "test.py"), 'w') as f:
+        f.write(trial_script)
+
+    env = '/home/lhertel/profiles/python3env.profile'
+    sge_options = '-N sherpaSchedTest -P arcus.p -q arcus-ubuntu.q -l hostname=\'(arcus-5)\''
+
+    s = sherpa.schedulers.SGEScheduler(environment=env,
+                                       submit_options=sge_options,
+                                       output_dir=test_dir)
+
+    job_id = s.submit_job("python {}/test.py".format(test_dir),
+                          env={'SHERPA_TRIAL_ID': 3, 'SHERPA_HOST': host})
+
+    try:
+        time.sleep(2)
+        assert s.get_status(job_id) == sherpa.schedulers._JobStatus.running
+
+        time.sleep(10)
+        testlogger.debug(s.get_status(job_id))
+        assert s.get_status(job_id) == sherpa.schedulers._JobStatus.finished
+
+    finally:
+        shutil.rmtree(test_dir)
+
+
+def test_local_scheduler(test_dir):
+    trial_script = "import time, os\n"
+    trial_script += "time.sleep(3)\n"
+    trial_script += "assert os.environ.get('SHERPA_TRIAL_ID') == '3'\n"
+
+    with open(os.path.join(test_dir, "test.py"), 'w') as f:
+        f.write(trial_script)
+
+    s = sherpa.schedulers.LocalScheduler()
+
+    job_id = s.submit_job("python {}/test.py".format(test_dir),
+                          env={'SHERPA_TRIAL_ID': '3'})
+
+    assert s.get_status(job_id) == sherpa.schedulers._JobStatus.running
+
+    time.sleep(5)
+    testlogger.debug(s.get_status(job_id))
+    assert s.get_status(job_id) != sherpa.schedulers._JobStatus.running
