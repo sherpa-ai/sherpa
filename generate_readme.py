95ae13764492028dd1e5ba53379dafead244f17b
colladou@uci.edu <colladou@uci.edu>
2018-04-25 23:00:52 -0700

    typo in coding text

--- generate_readme.py
+++ generate_readme.py
@@ -1,7 +1,7 @@
 import os
 
 
-filenames = ['welcome.rst', 'gettingstarted/installation.rst', 'gettingstarted/kerastosherpa.rst']
+filenames = ['welcome.rst', 'gettingstarted/installation.rst', 'gettingstarted/kerastosherpa.rst', 'gettingstarted/algorithms.rst']
 with open('README.rst', 'w') as outfile:
     for fname in filenames:
         with open(os.path.join('docs', fname)) as infile:
