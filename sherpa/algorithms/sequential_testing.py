b5e79a38bd5b1a2c9e57e47c39fb6a72dc39f89a
larshertel <lhertel@uci.edu>
2019-05-05 13:50:58 -0700

    seq testing files added

---
+++ sherpa/algorithms/sequential_testing.py
@@ -0,0 +1,182 @@
+import collections
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from sherpa.algorithms import Algorithm
+from sherpa.core import TrialStatus, AlgorithmState
+import pandas
+import math
+
+
+class SequentialTesting(Algorithm):
+    """
+    Implements repeated trials based on a
+    group sequential testing design.
+
+    Args:
+        algorithm (sherpa.algorithms.Algorithm): a Sherpa
+            algorithm to suggest parameter configurations.
+        K (int): the number of candidate configurations to
+            evaluate.
+        n (iterable): cumulative sample sizes at which to conduct
+            interim analyses.
+        P (float): between 0 and 1, defines the sequential
+            testing boundary as in
+            @article{kittelson1999unifying,
+              title={A unifying family of group sequential test designs},
+              author={Kittelson, John M and Emerson, Scott S},
+              journal={Biometrics},
+              volume={55},
+              number={3},
+              pages={874--882},
+              year={1999},
+              publisher={Wiley Online Library}
+            }
+    """
+
+    def __init__(self, algorithm, K, n=(3, 6, 9), P=0.5, alpha=0.05, verbose=False):
+        self.algorithm = algorithm
+        self.K = {1: K}
+        self.k = 0
+        self.t = 1
+        self.n = n
+        self.verbose = verbose
+        if len(n) > 1:
+            self.dn = {i + 1: t - s for i, (s, t) in
+                       enumerate(zip([0] + list(self.n), list(self.n)))}
+        else:
+            self.dn = {1: n}
+        self.P = P
+        self.T = len(n)
+        self.config_stack = []
+        self.configs_by_stage = collections.defaultdict(list)
+        assert list(self.n) == [3, 6,
+                                9] and P == 0.5 and alpha == 0.05, "Currently only n=(3,6,9), P=0.5, and alpha=0.05 is supported"
+        self.alpha_dash = [0.023175014834345, 0.023175014834345,
+                           0.023175014834345]
+        self.parameter_types = {}
+        # reading configs from results they may
+        # have the wrong type (e.g. float instead of int). This is a fix to
+        # deal with this for now
+
+    def get_suggestion(self, parameters, results=None, lower_is_better=True):
+        """
+        Variables:
+        Stages: T
+        Stage: t=1,2,3...
+        Config in stage: K={1: K, 2: tilde{k}, 3: tilde{k}, ...}
+        Config counter within stage: k=1,2,3...
+        Sample sizes for each stage: dn={1: n_stage_1, 2: n_stage_2, ...}
+        Repeats to evaluate: config_stack=[config1, config2, ...]
+        Configurations by stage: {1: all the configs stage 1, 2: all configs stage 2, ...}
+        """
+        if len(self.config_stack) == 0:
+            if self.k == self.K[self.t]:
+                # end of the stage
+                if self.t == self.T or self.K[self.t] == 1:
+                    # end of algorithm
+                    return AlgorithmState.DONE
+                else:
+                    if self._is_stage_done(results, stage=self.t,
+                                           num_trials_for_stage=self.K[self.t] *
+                                                   self.dn[self.t]):
+                        # if all trials finished, do testing
+                        # and append new trials
+
+                        self.configs_by_stage[
+                            self.t + 1] = self._get_best_configs(parameters,
+                                                                 results,
+                                                                 configs=
+                                                                 self.configs_by_stage[
+                                                                     self.t],
+                                                                 alpha=
+                                                                 self.alpha_dash[
+                                                                     self.t])
+                        self.t += 1
+                        self.K[self.t] = len(self.configs_by_stage[self.t])
+                        self.k = 1
+                        self.config_stack = [self.configs_by_stage[self.t][
+                                                 self.k - 1]] * self.dn[self.t]
+                    else:
+                        # otherwise wait for trials to finish
+                        if self.verbose:
+                            print(AlgorithmState.WAIT)
+                        return AlgorithmState.WAIT
+
+            else:
+                self.k += 1
+                if self.t == 1:
+                    new_config = self.algorithm.get_suggestion(parameters,
+                                                               results,
+                                                               lower_is_better)
+                    if len(self.parameter_types) == 0:
+                        self.parameter_types = {p.name: p.type
+                                                for p in parameters}
+                    self.configs_by_stage[self.t].append(new_config)
+                    self.config_stack = [new_config] * self.dn[self.t]
+                else:
+                    self.config_stack = [self.configs_by_stage[self.t][
+                                             self.k - 1]] * self.dn[self.t]
+
+        return dict({pname: self.parameter_types[pname](pvalue)
+                     for pname, pvalue in self.config_stack.pop().items()},
+                    **{'stage': self.t})
+
+    @staticmethod
+    def _is_stage_done(results, stage, num_trials_for_stage):
+        """
+        Checks whether all trials from the given stage have completed.
+        """
+        completed = results[results.Status == TrialStatus.COMPLETED]
+        this_stage = completed[completed.stage == stage]
+        return len(this_stage) == num_trials_for_stage
+
+    def _get_best_configs(self, parameters, results, configs, alpha=0.05):
+        """
+        Implements the testing procedure itself and returns the reduced set
+        of parameter configurations.
+        """
+        df = self._prep_df_for_linreg(parameters, results, configs)
+        l = 1
+        h = df.Rank.max()
+        p = h
+        while l != h:
+            lm = ols('Objective ~ C(Rank)', data=df.loc[df.Rank <= p, :]).fit()
+
+            pval = sm.stats.anova_lm(lm, typ=2).loc[:, "PR(>F)"].ix["C(Rank)"]
+            reject = pval < alpha
+            if reject:
+                h = p - 1
+            else:
+                l = p
+            p = math.ceil((l + h) / 2)
+
+        return df.loc[df.Rank <= p, :].loc[:,
+               [p.name for p in parameters]].drop_duplicates().to_dict(
+            'records')
+
+    @staticmethod
+    def _prep_df_for_linreg(parameters, results, configs):
+        """
+        Filter results corresponding to parameter configurations in `configs`
+        argument and create a grouping variable Rank that corresponds to
+        the rank of each group in terms of the average objective.
+
+        Args:
+            parameters (List[sherpa.Parameter]): list of parameters
+            results (pandas.DataFrame): current results
+            configs (List[Dict[Str, Any]]): parameter configurations that
+                correspond to groups.
+        """
+        param_names = [p.name for p in parameters]
+        completed = results[results.Status == TrialStatus.COMPLETED]
+        config_df = pandas.DataFrame(configs)
+        filtered_configs = completed.merge(config_df, how='inner',
+                                           on=param_names)
+        group_ranks = filtered_configs.groupby(param_names).agg('mean') \
+                          .loc[:, ['Objective']].rank(axis=0) \
+            .astype({'Objective': int}) \
+            .rename({'Objective': 'Rank'}, axis=1) \
+            .reset_index() \
+            .merge(filtered_configs)
+        return group_ranks
+
---
+++ tests/test_sequential_testing.py
@@ -0,0 +1,245 @@
+import collections
+import sherpa
+import pandas
+import tempfile
+import shutil
+import os
+import numpy
+from sherpa.algorithms import SequentialTesting
+
+
+def test_is_stage_done():
+    results_df = pandas.DataFrame(collections.OrderedDict(
+        [('Trial-ID', [1] * 1 + [2] * 1 + [3] * 1),
+         ('Status', [sherpa.TrialStatus.COMPLETED] * 3),
+         ('stage', [2, 2, 2] * 1),
+         ('a', [1, 1, 1] * 1),
+         ('b', [2, 2, 2] * 1),
+         ('Objective', [0.1] * 1 + [0.2] * 1 + [0.3] * 1)]
+    ))
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=4,
+                           n=(3, 6, 9),
+                           P=0.5)
+    assert gs._is_stage_done(results_df, stage=2,
+                             num_trials_for_stage=3) == True
+    assert gs._is_stage_done(results_df, stage=2,
+                             num_trials_for_stage=4) == False
+
+
+def test_prep_df_for_linreg():
+    parameters = [sherpa.Choice('a', [0, 1]),
+                  sherpa.Choice('b', [3, 4])]
+
+    configs = [{'a': 0, 'b': 3}, {'a': 0, 'b': 4}]
+
+    results_df = pandas.DataFrame(collections.OrderedDict(
+        [('Trial-ID', list(range(1, 9))),
+         ('Status', [sherpa.TrialStatus.COMPLETED] * 8),
+         ('stage', [1] * 8),
+         ('a', [0, 0, 0, 0, 1, 1, 1, 1]),
+         ('b', [3, 3, 4, 4, 3, 3, 4, 4]),
+         ('Objective', [1., 1.1, 2., 2.1, 5., 5.1, 6., 6.1])]
+    ))
+
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=4,
+                           n=(3, 6, 9),
+                           P=0.5)
+
+    r = gs._prep_df_for_linreg(parameters, results_df, configs)
+    cols = sorted(r.columns.tolist())
+
+    r_expected = results_df.loc[results_df.Objective < 5.]
+    r_expected = r_expected.assign(Rank=pandas.Series([1, 1, 2, 2]))
+    print(r)
+    print(r_expected)
+
+    assert r.loc[:, cols].equals(r_expected.loc[:, cols])
+
+
+def test_get_best_configs():
+    parameters = [sherpa.Choice('a', [0, 1]),
+                  sherpa.Choice('b', [3, 4])]
+
+    configs = [{'a': 0, 'b': 3},
+               {'a': 0, 'b': 4},
+               {'a': 1, 'b': 3},
+               {'a': 1, 'b': 4}]
+
+    results_df = pandas.DataFrame(collections.OrderedDict(
+        [('Trial-ID', list(range(1, 9))),
+         ('Status', [sherpa.TrialStatus.COMPLETED] * 8),
+         ('stage', [1] * 8),
+         ('a', [0, 0, 0, 0, 1, 1, 1, 1]),
+         ('b', [3, 3, 4, 4, 3, 3, 4, 4]),
+         ('Objective', [1., 1.1, 1.1, 1.2, 5., 5.1, 6., 6.1])]
+    ))
+
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=4,
+                           n=(3, 6, 9),
+                           P=0.5)
+
+    best_configs = gs._get_best_configs(parameters, results_df, configs,
+                                        alpha=0.05)
+    print(best_configs)
+    print(configs[0:2])
+    assert best_configs == configs[0:2]
+
+
+def test_get_suggestion():
+    for _ in range(10):
+        parameters = [sherpa.Continuous('myparam', [0, 1]),
+                      sherpa.Discrete('myparam2', [0, 10])]
+        rs = sherpa.algorithms.RandomSearch()
+        gs = SequentialTesting(algorithm=rs,
+                               K=10,
+                               n=(3, 6, 9),
+                               P=0.5)
+        study = sherpa.Study(algorithm=gs,
+                             parameters=parameters,
+                             lower_is_better=True,
+                             disable_dashboard=True)
+        seen_configs = []
+        last_config = {}
+        config_count = 3
+        for trial in study:
+            print(trial.id, trial.parameters, "{}/{}".format(gs.k, gs.K[gs.t]),
+                  "{}/{}".format(gs.t, gs.T))
+            if trial.parameters == last_config:
+                config_count += 1
+                assert config_count <= 3
+            elif trial.parameters == "DONE":
+                assert gs.K[gs.t] == 1 or gs.t == 3
+                break
+            else:
+                assert config_count == 3
+                config_count = 1
+                last_config = trial.parameters
+                if trial.id <= 30:
+                    seen_configs.append(trial.parameters['myparam'])
+                else:
+                    assert trial.parameters['myparam'] in seen_configs
+            study.add_observation(trial,
+                                  iteration=1,
+                                  objective=trial.parameters[
+                                                'myparam'] + numpy.random.normal(
+                                      scale=0.01))
+            study.finalize(trial)
+
+
+def test_type_I_error():
+    false_positives = 0
+    K = 50
+    nreps = 100
+    for _ in range(nreps):
+        parameters = [sherpa.Continuous('myparam', [0, 1])]
+        rs = sherpa.algorithms.RandomSearch()
+        gs = SequentialTesting(algorithm=rs,
+                               K=K,
+                               n=(3, 6, 9),
+                               P=0.5)
+        study = sherpa.Study(algorithm=gs,
+                             parameters=parameters,
+                             lower_is_better=True,
+                             disable_dashboard=True)
+        seen_configs = []
+        last_config = {}
+        config_count = 3
+        for trial in study:
+            study.add_observation(trial,
+                                  iteration=1,
+                                  objective=numpy.random.normal())
+            study.finalize(trial)
+        if gs.K.get(3, 0) < K:
+            false_positives += 1
+    print(float(false_positives) / nreps)
+    assert float(false_positives) / nreps == 0.05
+
+
+def test_wait():
+    parameters = [sherpa.Continuous('myparam', [0, 1])]
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=10,
+                           n=(3, 6, 9),
+                           P=0.5)
+    study = sherpa.Study(algorithm=gs,
+                         parameters=parameters,
+                         lower_is_better=True,
+                         disable_dashboard=True)
+
+    for _ in range(10*3 - 1):
+        trial = study.get_suggestion()
+        print(trial.id, trial.parameters, "{}/{}".format(gs.k, gs.K[gs.t]),
+              "{}/{}".format(gs.t, gs.T))
+        study.add_observation(trial,
+                              iteration=1,
+                              objective=trial.parameters['myparam'] + numpy.random.normal(
+                                  scale=0.01))
+        study.finalize(trial)
+
+    trial = study.get_suggestion()
+    assert trial.parameters['stage'] == 1
+
+    waittrial = study.get_suggestion()
+    assert waittrial == 'WAIT'
+    study.add_observation(trial,
+                          iteration=1,
+                          objective=trial.parameters['myparam'] + numpy.random.normal(
+                              scale=0.01))
+    study.finalize(trial)
+
+    trial = study.get_suggestion()
+    assert trial.parameters['stage'] == 2
+
+testscript = """import sherpa
+import time
+import numpy
+
+client = sherpa.Client()
+trial = client.get_trial()
+client.send_metrics(trial=trial, iteration=1,
+                    objective=numpy.random.normal())
+"""
+
+
+def test_parallel():
+    tempdir = tempfile.mkdtemp(dir=".")
+
+    parameters = [sherpa.Continuous('myparam', [0, 1])]
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=3,
+                           n=(3, 6, 9),
+                           P=0.5,
+                           verbose=True)
+
+    scheduler = sherpa.schedulers.LocalScheduler()
+
+    filename = os.path.join(tempdir, "test.py")
+    with open(filename, 'w') as f:
+        f.write(testscript)
+
+    try:
+        results = sherpa.optimize(parameters=parameters,
+                                  algorithm=gs,
+                                  lower_is_better=True,
+                                  filename=filename,
+                                  output_dir=tempdir,
+                                  scheduler=scheduler,
+                                  max_concurrent=2,
+                                  verbose=1,
+                                  disable_dashboard=True)
+
+    finally:
+        pass
+        # shutil.rmtree(tempdir)
+
+
+if __name__ == '__main__':
+    test_parallel()
