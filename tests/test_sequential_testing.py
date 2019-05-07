6c97f125ec7a3e2cc272ef0dd1f9c7ecda177d85
larshertel <lhertel@uci.edu>
2019-05-07 15:00:35 -0700

    overall tests; get_best_result test

--- tests/test_sequential_testing.py
+++ tests/test_sequential_testing.py
@@ -49,7 +49,7 @@ def test_prep_df_for_linreg():
                            n=(3, 6, 9),
                            P=0.5)
 
-    r = gs._prep_df_for_linreg(parameters, results_df, configs)
+    r = gs._prep_df_for_linreg(parameters, results_df, configs, lower_is_better=True)
     cols = sorted(r.columns.tolist())
 
     r_expected = results_df.loc[results_df.Objective < 5.]
@@ -165,33 +165,137 @@ def test_get_suggestion():
             study.finalize(trial)
 
 
-def test_type_I_error():
-    false_positives = 0
-    K = 50
-    nreps = 100
-    for _ in range(nreps):
-        parameters = [sherpa.Continuous('myparam', [0, 1])]
-        rs = sherpa.algorithms.RandomSearch()
-        gs = SequentialTesting(algorithm=rs,
-                               K=K,
-                               n=(3, 6, 9),
-                               P=0.5)
-        study = sherpa.Study(algorithm=gs,
-                             parameters=parameters,
-                             lower_is_better=True,
-                             disable_dashboard=True)
-        seen_configs = []
-        last_config = {}
-        config_count = 3
-        for trial in study:
-            study.add_observation(trial,
-                                  iteration=1,
-                                  objective=numpy.random.normal())
-            study.finalize(trial)
-        if gs.K.get(3, 0) < K:
-            false_positives += 1
-    print(float(false_positives) / nreps)
-    assert float(false_positives) / nreps == 0.05
+def test_overall_lower_is_better():
+    parameters = [sherpa.Continuous('myparam', [0, 10]),
+                  sherpa.Discrete('myparam2', [0, 10])]
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=10,
+                           n=(3, 6, 9),
+                           P=0.5)
+    study = sherpa.Study(algorithm=gs,
+                         parameters=parameters,
+                         lower_is_better=True,
+                         disable_dashboard=True)
+
+    for trial in study:
+        print(trial.id, trial.parameters, "{}/{}".format(gs.k, gs.K[gs.t]),
+              "{}/{}".format(gs.t, gs.T))
+
+        study.add_observation(trial,
+                              iteration=1,
+                              objective=trial.parameters[
+                                            'myparam'] + numpy.random.normal(
+                                  scale=1.))
+        study.finalize(trial)
+
+    completed = study.results.query("Status == 'COMPLETED'")
+    assert completed.myparam.min() in completed[completed.stage == 2].myparam.unique()
+
+
+def test_overall_larger_is_better():
+    parameters = [sherpa.Continuous('myparam', [0, 10]),
+                  sherpa.Discrete('myparam2', [0, 10])]
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=10,
+                           n=(3, 6, 9),
+                           P=0.5)
+    study = sherpa.Study(algorithm=gs,
+                         parameters=parameters,
+                         lower_is_better=False,
+                         disable_dashboard=True)
+
+    for trial in study:
+        print(trial.id, trial.parameters, "{}/{}".format(gs.k, gs.K[gs.t]),
+              "{}/{}".format(gs.t, gs.T))
+
+        study.add_observation(trial,
+                              iteration=1,
+                              objective=trial.parameters[
+                                            'myparam'] + numpy.random.normal(
+                                  scale=1.))
+        study.finalize(trial)
+
+    completed = study.results.query("Status == 'COMPLETED'")
+    assert completed.myparam.max() in completed[completed.stage == 2].myparam.unique()
+
+
+def test_get_best_result_lower():
+    parameters = [sherpa.Choice('a', [0, 1]),
+                  sherpa.Choice('b', [3, 4])]
+
+    results_df = pandas.DataFrame(collections.OrderedDict(
+        [('Trial-ID', list(range(1, 9))),
+         ('Status', [sherpa.TrialStatus.COMPLETED] * 8),
+         ('stage', [1] * 8),
+         ('a', [0, 0, 0, 0, 1, 1, 1, 1]),
+         ('b', [3, 3, 4, 4, 3, 3, 4, 4]),
+         ('Objective', [1., 1.1, 2.1, 2.2, 5., 5.1, 6., 6.1])]
+    ))
+
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=4,
+                           n=(3, 6, 9),
+                           P=0.5)
+
+    best_config = gs.get_best_result(parameters, results_df,
+                                      lower_is_better=True)
+    assert best_config == {'a': 0, 'b': 3, 'MeanObjective': 1.05}
+
+
+def test_get_best_result_larger():
+    parameters = [sherpa.Choice('a', [0, 1]),
+                  sherpa.Choice('b', [3, 4])]
+
+    results_df = pandas.DataFrame(collections.OrderedDict(
+        [('Trial-ID', list(range(1, 9))),
+         ('Status', [sherpa.TrialStatus.COMPLETED] * 8),
+         ('stage', [1] * 8),
+         ('a', [0, 0, 0, 0, 1, 1, 1, 1]),
+         ('b', [3, 3, 4, 4, 3, 3, 4, 4]),
+         ('Objective', [1., 1.1, 2.1, 2.2, 5., 5.1, 6., 6.1])]
+    ))
+
+    rs = sherpa.algorithms.RandomSearch()
+    gs = SequentialTesting(algorithm=rs,
+                           K=4,
+                           n=(3, 6, 9),
+                           P=0.5)
+
+    best_config = gs.get_best_result(parameters, results_df,
+                                      lower_is_better=False)
+    assert best_config == {'a': 1, 'b': 4, 'MeanObjective': 6.05}
+
+
+# def test_type_I_error():
+#     false_positives = 0
+#     K = 50
+#     nreps = 100
+#     for _ in range(nreps):
+#         parameters = [sherpa.Continuous('myparam', [0, 1])]
+#         rs = sherpa.algorithms.RandomSearch()
+#         gs = SequentialTesting(algorithm=rs,
+#                                K=K,
+#                                n=(3, 6, 9),
+#                                P=0.5)
+#         study = sherpa.Study(algorithm=gs,
+#                              parameters=parameters,
+#                              lower_is_better=True,
+#                              disable_dashboard=True)
+#         seen_configs = []
+#         last_config = {}
+#         config_count = 3
+#         for trial in study:
+#             study.add_observation(trial,
+#                                   iteration=1,
+#                                   objective=numpy.random.normal())
+#             study.finalize(trial)
+#         if gs.K.get(3, 0) < K:
+#             false_positives += 1
+#     print(float(false_positives) / nreps)
+#     assert float(false_positives) / nreps == 0.05
 
 
 def test_wait():
@@ -262,7 +366,7 @@ def test_parallel():
         results = sherpa.optimize(parameters=parameters,
                                   algorithm=gs,
                                   lower_is_better=True,
-                                  filename=filename,
+                                  command="python {}".format(filename),
                                   output_dir=tempdir,
                                   scheduler=scheduler,
                                   max_concurrent=2,
