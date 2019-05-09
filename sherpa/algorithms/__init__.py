ea483b4bb1bb8723ccaa8971057fb749c312aee1
larshertel <lhertel@uci.edu>
2019-05-09 12:52:17 -0700

    rm seq-testing

--- sherpa/algorithms/__init__.py
+++ sherpa/algorithms/__init__.py
@@ -19,4 +19,3 @@ along with SHERPA.  If not, see <http://www.gnu.org/licenses/>.
 """
 from .core import *
 from .successive_halving import SuccessiveHalving
-from .sequential_testing import SequentialTesting
